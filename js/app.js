2e171ee248986072ba3c4fac6fc48199f966f0ea
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-01 11:54:04 +0600

    Update details option

--- js/app.js
+++ js/app.js
@@ -10,15 +10,71 @@ const removeSection = getId => {
     element.classList.add('d-none');
 };
 
+const displayPhoneDetails = details => {
+    console.log(details);
+    const showPhoneDetails = document.getElementById('showPhoneDetails');
+    showPhoneDetails.textContent = '';
+    showPhoneDetails.innerHTML = `
+        <div class="card mb-3 w-75 mx-auto border-3 border-success">
+            <div class="row g-0">
+                <div class="col-md-4 text-center p-2">
+                    <img src="${details.image}" class="img-fluid rounded-start" alt="${details.name}_img">
+                </div>
+                <div class="col-md-8 bg-success">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">${details.name}</h5>
+                        <p class="card-text">Brand Name : ${details.brand}</p>
+                        <p class="card-text">Release Date : ${details.releaseDate}</p>
+                        <div class="w-100 p-2 bg-warning rounded">
+                            <h5 class="text-success">Main Features</h5>
+                            <span class="m-1 badge bg-success w-100 text-start text-wrap">Chip Set : ${details.mainFeatures.chipSet}</span>
+                            <span class="m-1 badge bg-success w-100 text-start text-wrap">Display Size : ${details.mainFeatures.displaySize}</span>
+                            <span class="m-1 badge bg-success w-100 text-start text-wrap">Memory : ${details.mainFeatures.memory}</span>
+                            <span class="m-1 badge bg-success w-100 text-start text-wrap">Storage : ${details.mainFeatures.storage}</span>
+                            <div class="m-1 badge bg-success w-100 text-start text-wrap">
+                                <h6>Sensors : </h6>
+                                <p id="sensor" class="text-warning"></p>
+                            </div>
+                            <div class="m-1 badge bg-success w-100 text-start text-wrap">
+                                <h6>Others : </h6>
+                                ${details.others ? `<p class="text-warning">
+                                    <span class="m-1 badge bg-warning text-success text-wrap">${'Bluetooth : ' + details.others.Bluetooth}</span>
+                                    <span class="m-1 badge bg-warning text-success text-wrap">${'GPS : ' + details.others.GPS}</span>
+                                    <span class="m-1 badge bg-warning text-success text-wrap">${'NFC : ' + details.others.NFC}</span>
+                                    <span class="m-1 badge bg-warning text-success text-wrap">${'Radio : ' + details.others.Radio}</span>
+                                    <span class="m-1 badge bg-warning text-success text-wrap">${'USB : ' + details.others.USB}</span>
+                                    <span class="m-1 badge bg-warning text-success text-wrap">${'WLAN : ' + details.others.WLAN}</span>
+                                </p>` : '<p class="text-warning text-wrap">This phone has no other information<p>'}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    const sensorElement = document.getElementById('sensor');
+    details.mainFeatures.sensors.forEach(sensor => {
+        const span = document.createElement('span');
+        span.innerText = sensor+', ';
+        sensorElement.appendChild(span);
+    })
+}
+
 // show phone details
 const viewPhoneDetails = async (getSlug) => {
+    showSection('loading');
     const url = `https://openapi.programming-hero.com/api/phone/${getSlug}`;
     const res = await fetch(url);
     const data = await res.json();
     if(data.status === true){
-        console.log(data);
+        removeSection('loading');
+        showSection('showPhoneDetails');
+        displayPhoneDetails(data.data);
     }else{
-        console.log(data.status);
+        removeSection('showPhoneDetails');
+        removeSection('showAllPhones');
+        removeSection('loading');
+        showSection('notFound');
     }
 }
 
