83ce4c88f15d76e70ff44adae4b3386e66c8f8b8
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-01 07:06:12 +0600

    Add not found fature

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 
+const showSection = getId => {
+    const element = document.getElementById(getId);
+    element.classList.remove('d-none');
+};
+
+const removeSection = getId => {
+    const element = document.getElementById(getId);
+    element.classList.add('d-none');
+};
+
+const displayAllPhones = phones => {
+    console.log(phones);
+}
+
 // call api for search all phones
 const loadPhones = async (getSearchText) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${getSearchText}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    if(data.status == true){
+        removeSection('notFound');
+        displayAllPhones(data);
+    }else{
+        removeSection('showPhoneDetails');
+        removeSection('showAllPhones');
+        showSection('notFound');
+    }
 };
 
 // onclick function for search btn
